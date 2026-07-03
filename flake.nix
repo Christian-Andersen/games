10ae1ffd8d218f27db17b86dd770fa45c290462c
Christian Andersen <cdandersen98@gmail.com>
2026-07-03 17:13:33 +1000

    addex nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Python and Frontend development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.permittedInsecurePackages = [ "pnpm-10.34.0" ];
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          biome
+          git
+          just
+          pnpm
+          prek
+          uv
+          vtsls
+          vue-language-server
+        ];
+      };
+    };
+}
